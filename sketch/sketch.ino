eaf2821f72104080a9923217a8f545e45fb1a54f
ENDREs12 <endrebogenes@gmail.com>
2026-09-16 09:19:18 +0200

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -350,6 +350,10 @@ void sjekkSerialMeny() {
   // TODO: Sjekk om brukeren har skrevet noe i Serial-monitoren, og åpne menyen med visMeny() hvis teksten er "meny"
   // Tips: Serial.available() forteller om det finnes tekst å lese
   // Tips: Serial.readStringUntil('\n') leser inn teksten, input.trim() fjerner mellomrom/linjeskift
+
+  if(Serial.available()){
+    String svar = Serial.readString();
+    Serial.println(svar);
 }
 
 //==================================================================================================================================
